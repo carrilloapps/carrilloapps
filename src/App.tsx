da14cfa4e5d11f5c3af83f8c69b06e14ce1d5011
Jose Carrillo <joseporfirio.carrilloechenique@externos-cl.cencosud.com>
2024-10-17 12:24:17 -0500

    💄 Add informacion + customize components

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,22 @@ export default function App() {
     <div className="min-h-screen">
       <TopBar simple />
       <div className="p-10 flex flex-col justify-center items-center">
-      <PresentationCard
-          greeting="José Carrillo"
+        <PresentationCard
+          name="José Carrillo"
           subtitle="Senior Software Engineering"
           imageUrl="https://avatars.githubusercontent.com/u/16759783?v=4"
           links={{
-            instagram: 'https://www.instagram.com/fluxstudio.ve',
-            facebook: 'https://www.facebook.com/fluxstudio.ve',
-            whatsapp: 'https://wa.me/+584127786458',
+            github: 'https://github.com/carrilloapps',
+            xTwitter: 'https://x.com/carrilloapps',
+            instagram: 'https://instagram.com/carrillo.apps',
+            facebook: 'https://facebook.com/carrillo.apps',
+            tiktok: 'https://tiktok.com/@carrillo.apps',
+            linkedin: 'https://www.linkedin.com/in/carrilloapps',
           }}
         />
-      <h1 className="text-3xl font-bold text-white">
-        Hello world!
-      </h1>
+        <h1 className="text-3xl font-bold text-white">
+          Hello world!
+        </h1>
       </div>
     </div>
   );
--- src/components/PresentationCard/PresentationCard.tsx
+++ src/components/PresentationCard/PresentationCard.tsx
@@ -1,43 +1,91 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXTwitter, faGithub, faInstagram, faTiktok, faFacebookF, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 interface Props {
   imageUrl: string;
-  greeting: string;
+  name: string;
   subtitle: string;
   links?: {
+    github?: string;
+    xTwitter?: string;
     instagram?: string;
     facebook?: string;
-    whatsapp?: string;
+    tiktok?: string;
+    linkedin?: string;
   };
 }
 
-export default function PresentationCard({ imageUrl, greeting, subtitle, links }: Props) {
+export default function PresentationCard({ imageUrl, name, subtitle, links }: Props) {
   return (
-    <div className="rounded-lg mb-10 w-120 text-center flex flex-col items-center text-white">
-      <div className="border border-gray-700 rounded-full mb-5">
-        <img src={imageUrl} alt="Avatar" className="rounded-full w-48 h-48 bg-indigo-50" />
-      </div>
-      <h1 className="text-3xl font-bold font-montserrat">{greeting}</h1>
-      <p className="mt-2 mb-4 text-sm font-light text-orange-200">{subtitle}</p>
-      {links ? (
-        <div className="flex justify-center space-x-3">
-          {links.instagram ? (
-            <a href={links.instagram} target="_blank" rel="noreferrer">
-              <div>*</div>
-            </a>
-          ) : null}
-          {links.facebook ? (
-            <a href={links.facebook} target="_blank" rel="noreferrer">
-              <div>*</div>
-            </a>
-          ) : null}
-          {links.whatsapp ? (
-            <a href={links.whatsapp} target="_blank" rel="noreferrer">
-              <div>*</div>
-            </a>
+    <div className="rounded-lg mb-10 w-120 text-center flex flex-col items-center text-gray-100 max-w-[520px]">
+      <div className="mb-5 flex flex-row gap-4 align-center min-w-[100%]">
+        <img src={imageUrl} alt="Avatar" className="border-4 border-orange-200 rounded-full w-24 h-24 bg-indigo-50" />
+        <div className="flex flex-col items-start">
+          <h1 className="text-3xl font-bold font-montserrat text-white">{name}</h1>
+          <p className="mt-1 text-sm font-light text-orange-200">{subtitle}</p>
+          {links ? (
+            <div className="items-start mt-3 flex flex-row justify-center space-x-3 md:space-x-6">
+              {links.github ? (
+                <a href={links.github} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faGithub} className="h-5 w-auto" />
+                </a>
+              ) : null}
+              {links.xTwitter ? (
+                <a href={links.xTwitter} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faXTwitter} className="h-5 w-auto" />
+                </a>
+              ) : null}
+              {links.instagram ? (
+                <a href={links.instagram} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faInstagram} className="h-5 w-auto" />
+                </a>
+              ) : null}
+              {links.facebook ? (
+                <a href={links.facebook} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faFacebookF} className="h-5 w-auto" />
+                </a>
+              ) : null}
+              {links.tiktok ? (
+                <a href={links.tiktok} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faTiktok} className="h-5 w-auto" />
+                </a>
+              ) : null}
+              {links.linkedin ? (
+                <a href={links.linkedin} target="_blank" rel="noreferrer">
+                  <FontAwesomeIcon icon={faLinkedinIn} className="h-5 w-auto" />
+                </a>
+              ) : null}
+            </div>
           ) : null}
         </div>
-      ) : null}
+      </div>
+      <div className="flex flex-row gap-5 text-sm justify-between items-stretch">
+        <div className="flex flex-col md:flex-row md:gap-1">
+          <strong className="text-orange-200">9+</strong> años de experiencia
+        </div>
+        <div className="flex flex-col md:flex-row md:gap-1">
+          <strong className="text-orange-200">100+</strong> proyectos realizados
+        </div>
+        <div className="flex flex-col md:flex-row md:gap-1">
+          <strong className="text-orange-200">10K+</strong> seguidores totales
+        </div>
+      </div>
+      <div className="mt-5 min-w-[100%]">
+        <a className="flex flex-row gap-x-5 p-2 px-5 pb-4 items-center border-2 border-red-400 hover:bg-red-400 transition duration-150 rounded-xl" href="#">
+          <FontAwesomeIcon icon={faYoutube} size="xl" />
+          <div className="text-start">
+            <div className="text-sm md:text-lg font-bold text-white">Próxima transmisión en vivo</div>
+            <div className="text-xs text-orange-200 text-ellipsis">Sábado 2 de noviembre del 2024 | Zona horario: Bogotá</div>
+          </div>
+        </a>
+      </div>
+      <div className="mt-5 max-w-[100%] text-left text-ellipsis">
+        <p>Soy ingeniero de software y actualmente trabajo como <strong>Freelancer 
+          Full-Stack Developer</strong> para iOS y Android con <strong>React Native</strong>.
+          Además, <em>creo contenido formativo sobre programación</em> en redes sociales y hago Streams
+          en mi canal de YouTube cada semana.</p>
+      </div>
     </div>
   );
 }
