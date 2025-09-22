97ac75c332d8f492ef227894f273353a56200eaa
Jose Carrillo <soyjrcarrillo@gmail.com>
2025-09-22 07:11:44 -0500

    fix: correct capitalization in section headings

--- app/page.tsx
+++ app/page.tsx
@@ -168,7 +168,7 @@ export default function Home() {
                     <path fillRule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clipRule="evenodd" />
                     <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
                   </svg>
-                  Trayectoria & Experiencia
+                  Trayectoria & experiencia
                 </div>
               </motion.div>
               <motion.h2
@@ -323,7 +323,7 @@ export default function Home() {
                   <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                  Certificaciones & Habilidades
+                  Certificaciones & habilidades
                 </div>
               </motion.div>
               <motion.h2
@@ -726,7 +726,7 @@ export default function Home() {
                   <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                     <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z" />
                   </svg>
-                  Portafolio & Proyectos
+                  Portafolio & proyectos
                 </div>
               </motion.div>
               <motion.h2
