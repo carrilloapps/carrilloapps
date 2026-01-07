32e8295348372b7c1b259cb2ad68a1dfda19ddcd
José Carrillo <soyjrcarrillo@gmail.com>
2026-01-07 08:23:09 -0500

    feat: Add caching headers for CSS files to improve performance and reduce load times

--- next.config.mjs
+++ next.config.mjs
@@ -205,6 +205,23 @@ const nextConfig = {
         ],
       },
       {
+        source: '/_next/static/css/:path*.css',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/css; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+          {
+            key: 'CDN-Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
         source: '/_next/static/:path*',
         headers: [
           {
